69fff14ff9281cf00445e67e00a3232c7c97cb03
burakozcan <burakozcan@burakozcan-MacBook-Pro.local>
2025-10-26 14:20:39 +0300

    prisma edit

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "./generated/prisma/index";
+import { PrismaClient } from "./generated/prisma";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
